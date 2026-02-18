e08f361d11725cdae0350329b133b07e6a67f267
a-naitdayaa <a.naitdayaa@void.fr>
2026-02-18 17:12:16 +0000

    fix : the url /goodby and added controller to the route /hello

--- src/app.php
+++ src/app.php
@@ -4,7 +4,10 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
 $routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
+$routes->add('hello', new Route('/hello/{name}', [
+    'name' => 'World',
+    '_controller' => 'render_template'
+]));
 $routes->add('bye', new Route('/bye'));
 
 return $routes;
